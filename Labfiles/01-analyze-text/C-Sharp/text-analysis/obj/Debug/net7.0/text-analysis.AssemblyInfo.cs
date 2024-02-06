a8901cd84ab5057d31b29d6d326af442d1f561ac
mpdacey <80190692+mpdacey@users.noreply.github.com>
2024-02-07 10:50:57 +1100

    Added missing dotnet7 files.

--- Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
+++ Labfiles/01-analyze-text/C-Sharp/text-analysis/obj/Debug/net7.0/text-analysis.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ffaec45fc4c709dfd78424961f89b9bf43f5734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81b1675a930c59b925389ee572881adbd46939ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("text-analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
